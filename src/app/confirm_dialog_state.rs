b0327c97691db59ddcefbce31d159f8bd23b77c1
riii111 <ryo.ichinose@nodex.inc>
2026-01-10 16:05:35 +0900

    feat(app): add empty state structs for connection modes

---
+++ src/app/confirm_dialog_state.rs
@@ -0,0 +1,4 @@
+#[derive(Debug, Clone, Default)]
+pub struct ConfirmDialogState {
+    // Fields to be implemented in PR2
+}
---
+++ src/app/connection_error_state.rs
@@ -0,0 +1,4 @@
+#[derive(Debug, Clone, Default)]
+pub struct ConnectionErrorState {
+    // Fields to be implemented in PR3
+}
