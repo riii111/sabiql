abe87a3109aed38270fbba6359b818223ec99c59
riii111 <ryo.ichinose@nodex.inc>
2026-01-11 17:10:45 +0900

    refactor: extract metadata sub-reducer

--- src/app/reducers/mod.rs
+++ src/app/reducers/mod.rs
@@ -1,5 +1,6 @@
 mod connection;
 mod helpers;
+mod metadata;
 mod modal;
 mod navigation;
 mod sql_modal;
@@ -8,6 +9,7 @@ pub use connection::reduce_connection;
 pub use helpers::{
     char_count, char_to_byte_index, insert_char_at_cursor, validate_all, validate_field,
 };
+pub use metadata::reduce_metadata;
 pub use modal::reduce_modal;
 pub use navigation::reduce_navigation;
 pub use sql_modal::reduce_sql_modal;
