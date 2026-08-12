6c91adfb7dadbb6616b706faadf2ba5a78f39e38
riii111 <riii111@users.noreply.github.com>
2026-08-12 19:09:27 +0900

    chore: bump version to v2.0.0

--- flake.nix
+++ flake.nix
@@ -60,7 +60,7 @@
         {
           default = rustPlatform.buildRustPackage {
             pname = "sabiql";
-            version = "1.11.0";
+            version = "2.0.0";
 
             src = self;
             cargoLock.lockFile = ./Cargo.lock;
